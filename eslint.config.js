0632cd658f87adb4599296d612a5e16b99c80458
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-09-23 19:47:04 -0400

    Add: husky, prettier & eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'node_modules', '*.config.js', '*.config.cjs']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
@@ -14,6 +14,7 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      "prettier",
     ],
     languageOptions: {
       ecmaVersion: 2020,
