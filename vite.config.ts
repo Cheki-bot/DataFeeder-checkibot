9b3fe466289934443694d67c1591ca9c2d70fcb5
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-10-15 17:48:53 -0400

    Add: alias path

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': '/src',
+      '@components': '/src/components',
+      '@pages': '/src/pages',
+      '@assets': '/src/assets',
+      '@hooks': '/src/hooks',
+      '@utils': '/src/lib/utils',
+      '@lib': '/src/lib',
+    },
+  }
 })
