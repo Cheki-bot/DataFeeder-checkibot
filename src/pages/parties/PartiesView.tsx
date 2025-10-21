129e764de48bf429eb38e5e8ce720bf08c66253b
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-10-21 19:06:25 -0400

    Add: title for PartiesView

--- src/pages/parties/PartiesView.tsx
+++ src/pages/parties/PartiesView.tsx
@@ -5,35 +5,39 @@ import style from './PartiesView.module.css';
 export const PartiesView = () => {
     return (
         <div className={style.container}>
-            <CardComponent
-                description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
-                headerImageUrl="https://picsum.photos/300/300"
-                mainImageUrl="https://picsum.photos/2000/3000"
-                subtitle="Sample Subtitle"
-                title="Sample Title"
-            />
-            <CardComponent
-                description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
-                headerImageUrl="https://picsum.photos/300/300"
-                mainImageUrl="https://picsum.photos/2000/3000"
-                subtitle="Sample Subtitle"
-                title="Sample Title"
-            />
-            <CardComponent
-                description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
-                headerImageUrl="https://picsum.photos/300/300"
-                mainImageUrl="https://picsum.photos/2000/3000"
-                subtitle="Sample Subtitle"
-                title="Sample Title"
-            />
-            <CardComponent
-                description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
-                headerImageUrl="https://picsum.photos/300/300"
-                mainImageUrl="https://picsum.photos/2000/3000"
-                subtitle="Sample Subtitle"
-                title="Sample Title"
-                forAddCard
-            />
+            <h1 className={style.title}>Parties</h1>
+            <div className={style.cardContainer}>
+                <CardComponent
+                    description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
+                    headerImageUrl="https://picsum.photos/300/300"
+                    mainImageUrl="https://picsum.photos/2000/3000"
+                    subtitle="Sample Subtitle"
+                    title="Sample Title"
+                />
+                <CardComponent
+                    description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
+                    headerImageUrl="https://picsum.photos/300/300"
+                    mainImageUrl="https://picsum.photos/2000/3000"
+                    subtitle="Sample Subtitle"
+                    title="Sample Title"
+                />
+                <CardComponent
+                    description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
+                    headerImageUrl="https://picsum.photos/300/300"
+                    mainImageUrl="https://picsum.photos/2000/3000"
+                    subtitle="Sample Subtitle"
+                    title="Sample Title"
+                />
+                
+                <CardComponent
+                    description="This is a sample description for the card componensample description for the card componensample description for the card componensample description for the card component. lorem ipsum dolor sit amet, consectetur adipiscing elit. sadsadsasa s s s s s"
+                    headerImageUrl="https://picsum.photos/300/300"
+                    mainImageUrl="https://picsum.photos/2000/3000"
+                    subtitle="Sample Subtitle"
+                    title="Sample Title"
+                    forAddCard
+                />
+            </div>
         </div>
     );
 };
