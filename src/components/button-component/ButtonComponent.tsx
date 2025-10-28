75da0941a2ee116903dc9ffd94f18ce75dccb75a
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-10-28 18:23:22 -0400

    Add: title & fixed  styles

--- src/components/button-component/ButtonComponent.tsx
+++ src/components/button-component/ButtonComponent.tsx
@@ -1,27 +1,34 @@
-import style from "./ButtonComponent.module.css";
+import style from './ButtonComponent.module.css';
 
 interface ButtonComponentProps {
-  label?: string;
-  onClick?: () => void;
-  children?: React.ReactNode;
-  type?: "button" | "submit" | "reset";
-  onlyIcon?: boolean;
+    label?: string;
+    onClick?: () => void;
+    children?: React.ReactNode;
+    type?: 'button' | 'submit' | 'reset';
+    onlyIcon?: boolean;
+    danger?: boolean;
 }
-
 export const ButtonComponent = ({
-  label,
-  onClick,
-  children,
-  type = "button",
-  onlyIcon = false,
+    label,
+    onClick,
+    children,
+    type = 'button',
+    onlyIcon = false,
+    danger = false,
 }: ButtonComponentProps) => {
-  return (
-    <button
-      className={onlyIcon ? style.onlyIcon : style.button}
-      onClick={onClick}
-      type={type}
-    >
-      {children ? children : label}
-    </button>
-  );
+    return (
+        <button
+            className={
+                danger
+                    ? `${style.button} ${style.danger}`
+                    : onlyIcon
+                    ? style.onlyIcon
+                    : style.button
+            }
+            onClick={onClick}
+            type={type}
+        >
+            {children ? children : label}
+        </button>
+    );
 };
--- src/pages/candidates/CandidatesView.tsx
+++ src/pages/candidates/CandidatesView.tsx
@@ -1,11 +1,35 @@
-import { InputComponent } from '@/components';
+import { ButtonComponent, InputComponent } from '@/components';
 import { ListComponent } from '@/components/list-component/ListComponent';
 import { SearchComponent } from '@/components/search-component/SearchComponent';
 
 import style from './CandidatesView.module.css';
 
 const CandidatesView = () => {
-    const candidates = ['Alice', 'Bob', 'Charlie'];
+    const candidates = [
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+        'Alice',
+        'Bob',
+        'Charlie',
+        'David',
+        'Eve',
+    ];
+
+    const handleAddCandidate = (candidate: string) => {
+        console.log(`Adding candidate: ${candidate}`);
+    }
+
+    const handleRemove = (candidates: string[]) => {
+        console.log(`Removing candidates: ${candidates.join(', ')}`);
+    };
     return (
         <div className={style.container}>
             <h1>Candidates</h1>
@@ -15,13 +39,33 @@ const CandidatesView = () => {
                         <p>Esta activo</p>
                         <CustomCheckbox />
                     </div> */}
+                    <h3>Información del Candidato</h3>
                     <InputComponent label="Nombre Completo" type="text" />
                     <InputComponent label="Posición" type="text" />
-                    <InputComponent label='Estado del candidato' type='select' options={[ "option1", "option2" ]}/>
+                    <InputComponent
+                        label="Estado del candidato"
+                        type="select"
+                        options={['option1', 'option2']}
+                    />
+                    <span className={style.buttonContainer}>
+                        <ButtonComponent
+                            label="Agregar Candidato"
+                            type="button"
+                            onClick={() => handleAddCandidate("candidate")}
+                        />
+                    </span>
                 </form>
                 <div className={style.listContainer}>
                     <SearchComponent />
                     <ListComponent items={candidates} />
+                    <span className={style.buttonContainer}>
+                        <ButtonComponent
+                            label="Eliminar"
+                            type="button"
+                            danger
+                            onClick={() => handleRemove(candidates)}
+                        />
+                    </span>
                 </div>
             </div>
         </div>
