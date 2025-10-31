8dafb9c579cab407322508759ee21205f8614114
Claudio Mancilla <claudio.tech.edu@gmail.com>
2025-10-30 22:57:51 -0400

    feat: Header as been added

--- src/components/header-component/HeaderComponent.tsx
+++ src/components/header-component/HeaderComponent.tsx
@@ -1,3 +1,12 @@
+import { InputComponent } from '../input-component/InputComponent';
+import style from './HeaderComponent.module.css';
 export const HeaderComponent = () => {
-    return <h1>Header De claudio</h1>
-}
+    return (
+        <header className={style.header}>
+            <h1>Checkibot</h1>
+            <nav>
+                <InputComponent type='text' label='Busqueda' placeholder='Buscar'/>
+            </nav>
+        </header>
+    );
+};
