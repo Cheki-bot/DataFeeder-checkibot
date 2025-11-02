38ff53ca41c2f0b2077a7d0e543d46750c15257f
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-11-01 21:54:02 -0400

    Add: modal finished

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,12 @@
+import { LoginView, PartiesView, SignUpView } from '@pages/index'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter } from 'react-router'
 import { RouterProvider } from 'react-router/dom'
-import './index.css'
 import App from './App.tsx'
-import { ProtectedRoute } from './ProtectedRoute.tsx'
-import { PartiesView, LoginView, SignUpView } from '@pages/index'
+import './index.css'
 import CandidatesView from './pages/candidates/CandidatesView.tsx'
+import { ProtectedRoute } from './ProtectedRoute.tsx'
 
 const router = createBrowserRouter([
   {
