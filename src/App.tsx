fe35c777dd6dc51e4c537b84495de79e7440a7e8
Diego Perez <diego.fernando.perez.luna@gmail.com>
2025-11-01 22:27:03 -0400

    Add: header styles & type

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,11 @@
 import './App.css';
 import { HeaderComponent } from './components/header-component/HeaderComponent';
-import { InputComponent } from './components/input-component/InputComponent';
 import { LoginView } from './pages';
 
 function App() {
     return (
         <>
-            <HeaderComponent />
-            <InputComponent
-                type="text"
-                label="Input"
-                placeholder="Enter text here"
-            />
+            <HeaderComponent type='simple'/>
             <LoginView />
         </>
     );
