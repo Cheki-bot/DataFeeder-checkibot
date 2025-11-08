1050d8d942331c2daa8b78e6aa1bb93e111089c3
Steven Joel Ramos Salazar <stevenjramossalazar@gmail.com>
2025-11-08 00:56:46 -0400

    refactor: Remove unnecessary comment

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -8,7 +8,6 @@ export const api = axios.create({
   },
 });
 
-// Interceptor para agregar el token JWT a todas las peticiones
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
