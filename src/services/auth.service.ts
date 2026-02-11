842c10ab538a3d5ac361c34bbb6f4381ded0ecb4
Ricardo Ramirez <ricardo@richixs.dev>
2026-02-10 21:37:19 -0400

    feat(profile): add profile services

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,12 @@
 import { api } from './api.service';
-import type { RegisterFormData, UserFormData } from '@pages/auth/schemas/userSchema';
+import type {
+    ChangePasswordPayload,
+    ProfileResponse,
+} from '@/interfaces/Auth';
+import type {
+    RegisterFormData,
+    UserFormData,
+} from '@pages/auth/schemas/userSchema';
 
 export const login = async (userData: UserFormData) => {
     const data = await api.post('/auth/login', userData);
@@ -9,4 +16,14 @@ export const login = async (userData: UserFormData) => {
 export const register = async (userData: RegisterFormData) => {
     const { data } = await api.post('/auth/register', userData);
     return data;
+};
+
+export const getProfile = async () => {
+    const response = await api.get<ProfileResponse>('/auth/me');
+    return response.data;
+};
+
+export const changePassword = async (payload: ChangePasswordPayload) => {
+    const response = await api.patch('/auth/me/change-password', payload);
+    return response.data;
 };
